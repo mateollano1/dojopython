0ed0e9d93e5e35e3fd618a43f8c2ffdb6eecf6da
Dll28 <11658011+Dll28@users.noreply.github.com>
2020-03-06 17:07:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         'flask_sqlalchemy',
         'flask_login',
         'flask_wtf',
-        'pandas==0.25.0',
+        'pandas==0.24.2',
         'plotly==3.10.0',
         'scipy',
         'xlrd',
